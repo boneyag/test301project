822b7d6564218ca262f9998b944b9ba580506584
Akalanka <8133713+boneyag@users.noreply.github.com>
2024-06-04 11:48:32 -0600

    Update seaborn_facetgrid_misuse.py
    incorrect dataset name

--- seaborn_facetgrid_misuse.py
+++ seaborn_facetgrid_misuse.py
@@ -1,5 +1,5 @@
 import seaborn as sns
 
 df = sns.laod_dataset('tips')
-g = sns.FacetGrid(data='tips', col='time', row='sex')
+g = sns.FacetGrid(data='df', col='time', row='sex')
 g.map(sns.lmplot, 'total_bill', 'tip')
