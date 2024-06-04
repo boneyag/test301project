5f82a1198e540ca4c336558b05f93b42198798ab
Akalanka <8133713+boneyag@users.noreply.github.com>
2024-06-04 12:17:23 -0600

    Update seaborn_facetgrid_misuse.py

--- seaborn_facetgrid_misuse.py
+++ seaborn_facetgrid_misuse.py
@@ -1,5 +1,5 @@
 import seaborn as sns
 
 df = sns.laod_dataset('tips')
-g = sns.FacetGrid(data=df, col='time', row='sex')
-g.map(sns.lmplot, 'total_bill', 'tip')
+sns.lmplot(data=df, x='total_bill', y='tip', 
+           col='time', row='sex')
